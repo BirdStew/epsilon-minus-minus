0f0a180e2e0763b11849d721f111555cb393cb50
jhbettke@gmail.com <jhbettke@gmail.com@63e8670f-c1fa-dcbf-4e53-84b8e2d83b4c>
2011-02-14 15:02:43 +0000

    Added matrix module

---
+++ emm-core/matrix.c
@@ -0,0 +1,24 @@
+/*
+ * matrix.c
+ *
+ *  Created on: Feb 14, 2011
+ *      Author: James Bettke
+ */
+#include <stdio.h>
+
+struct matrix
+{
+	int rows;
+	int cols;
+	char* data;
+};
+
+typedef struct matrix matrix;
+
+
+
+
+int main( int argc, const char* argv[] )
+{
+	printf( "\nHello World\n\n" );
+}
