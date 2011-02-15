98838b36d369cb57ee65edf1d6621e19a8fa5b2c
jhbettke@gmail.com <jhbettke@gmail.com@63e8670f-c1fa-dcbf-4e53-84b8e2d83b4c>
2011-02-15 16:23:45 +0000

    Added usage to main.c

--- emm-core/main.c
+++ emm-core/main.c
@@ -8,9 +8,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printUsage();
+
+
 
 int main( int argc, const char** argv )
 {
-	printf( "\nHello World\n\n" );
+	printUsage();
 	return EXIT_SUCCESS;
+}
+
+
+void printUsage()
+{
+	char* usage=""
+	" Usage: emm-core [options] <message>\n"
+	"\n"
+	"Options:\n"
+	" -L                use linear encoding\n"
+	" -R                use Reed–Muller encoding\n"
+	" -H                use Hamming encoding\n"
+	" -h                print help.\n"
+	" -k <number>       number of original bits\n"
+	" -n <number>       number of encoded bits\n"
+	"\n"
+	"Notes:\n"
+	" <number> can be a single be a unsigned integer or a colon separated range.\n"
+	" <message> is the path to the mesage file.\n";
+
+	printf("%s", usage);
 }
