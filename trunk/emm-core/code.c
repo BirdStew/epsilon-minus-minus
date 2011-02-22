c61a9383bfab096e5db1037712770d60831900c3
jhbettke@gmail.com <jhbettke@gmail.com@63e8670f-c1fa-dcbf-4e53-84b8e2d83b4c>
2011-02-22 16:39:55 +0000

    Added new PCM functions and newCode in code.c

--- trunk/emm-core/code.c
+++ trunk/emm-core/code.c
@@ -4,14 +4,42 @@
  *  Created on: Feb 17, 2011
  *      Author: James Bettke
  */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include "code.h"
-#include <stdio.h> //FIXME
+#include <math.h>	// used for pow()
+#include <time.h>	// Used for Rand()
 
 
-Code* newCode()
+Code* newCode(int wordLen, int parityLen, int parityType)
 {
+	Matrix* tempPCM;
+
+	switch(parityType)
+	{
+		case DENSE:
+			tempPCM = newDenseParity(wordLen, parityLen);
+			break;
+		case LOW_DENSITY:
+			tempPCM = newLowDensityParity(wordLen, parityLen);
+			break;
+		case RANDOM:
+			tempPCM = newRandomParity(wordLen, parityLen);
+			break;
+		default:
+			fprintf(stderr, "error: Invalid parity type '%d' in 'newCode'\n", parityType);
+			exit(EXIT_FAILURE);
+	}
+
 	Code* c = (Code*)malloc(sizeof(Code));
+	c->wordLen = wordLen;
+	c->parityLen = parityLen;
+	c->generator = newGeneratorMatrix(tempPCM);
+	c->control =  newControlMatrix(tempPCM);
+	c->syndrome = newSyndromeMatrix(wordLen, parityLen);
+
+	delMatrix(&tempPCM);
 
 	return c;
 }
@@ -48,9 +76,17 @@ Matrix* newControlMatrix(Matrix* pcm)
 }
 
 
+Matrix* newSyndromeMatrix(int wordLen, int parityLen)
+{
+	Matrix* syn = newMatrix((int)pow(2,parityLen), wordLen + parityLen);
+	fprintf(stderr, "error: newSyndromeMatrix Unimplemented!");
+	exit(1);
+	return syn;
+}
+
 
 /*
- * Creates a matrix of dimension  rows, cols and fills it with ones.
+ * Creates a matrix of dimension rows, cols and fills it with ones.
  * Starting from the upper left, the diagonal is set to zero's.  Although
  * this function creates an inverted identity, it does not need to be a square
  * matrix.  The result is intended to be used as a parity check matrix.
@@ -65,19 +101,39 @@ Matrix* newDenseParity(int rows, int cols)
 	{
 		for(j = 0; j < cols; j++)
 		{
-			if(i == j)
-				d->data[i * cols + j] = 0;
-			else
+			if(i != j)
+			{
 				d->data[i * cols + j] = 1;
+			}
 		}
 	}
 	return d;
 }
 
 
-Matrix* newLowDenseParity(int rows, int cols)
+Matrix* newLowDensityParity(int rows, int cols)
 {
 
 	return 0;
 }
 
+
+/*
+ * Creates and returns matrix of random zeros and ones of
+ * dimension rows, cols. This is one of the methods of creating
+ * a parity check matrix.
+ */
+
+Matrix* newRandomParity(int rows, int cols)
+{
+		Matrix* m = newMatrix(rows, cols);
+		int i;
+		int n = rows * cols;
+
+		srand(time(NULL));
+		for(i = 0; i < n; i++)
+		{
+			m->data[i] = rand() % 2;
+		}
+		return m;
+}
