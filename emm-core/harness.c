a85c2da732b6e583b5d88c295c7274c1fb2977af
jhbettke@gmail.com <jhbettke@gmail.com@63e8670f-c1fa-dcbf-4e53-84b8e2d83b4c>
2011-02-23 23:33:02 +0000

    Added 'transmit' to harness.c/h

--- emm-core/harness.c
+++ emm-core/harness.c
@@ -5,3 +5,16 @@
  *      Author: James Bettke
  */
 
+
+void transmit(Matrix* packet, double errorProbability)
+{
+	int i;
+	int n = packet->rows + packet->cols;
+	for(i = 0; i < n; i++)
+	{
+		if(rand() % 1 <= errorProbability)
+		{
+			packet->data[i] ^= 1;
+		}
+	}
+}
