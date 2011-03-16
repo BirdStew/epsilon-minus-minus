cbd68ebf583b71339f011340a56389f659a42dde
jhbettke@gmail.com <jhbettke@gmail.com@63e8670f-c1fa-dcbf-4e53-84b8e2d83b4c>
2011-03-16 00:58:27 +0000

    changed output files to have the original file extension of the message instead of ".msg"

--- trunk/emm-core/harness.c
+++ trunk/emm-core/harness.c
@@ -72,7 +72,7 @@ void runHarness(int* wordLen, int* parityLen, double errorProb, int parityFlags,
 					clock_t endCodeExec = clock();
 
 					/* write decoded message to file */
-					sprintf(pathBuffer, "%s/%d-%d-%d-%d.msg", outPath, w, p, pType, (int)(errorProb*100));
+					sprintf(pathBuffer, "%s/%d-%d-%d-%d%s", outPath, w, p, pType, (int)(errorProb*100), strrchr(msgPath,'.'));
 					saveMessage(decodedMsg, pathBuffer);
 
 					/* save execution measures in stats struct */
